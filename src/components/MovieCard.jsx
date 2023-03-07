3b1a4d09cba1a99ea8c5e588a8344a889f2c1621
Wael Barhoumi <wael.barhoumi14@gmail.com>
2023-03-07 21:40:35 +0100

    small change

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -5,7 +5,7 @@ const MovieCard = ({movie}) => {
         <div className="movie-card">
             <img src={movie.poster} alt={movie.title} width={"200px"}/>
             <h2>{movie.title}</h2> <span>({movie.year})</span>
-            <h6>IMDB rating: {movie.rating}</h6>
+            <h6>IMDB rating: {movie.rating} /10</h6>
         </div>
     )
 }
